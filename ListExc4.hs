d29de96272476ddff7c1890fc438d737395d2b57
FilipBl4gojevic <84703395+FilipBl4gojevic@users.noreply.github.com>
2021-05-27 14:56:30 +0200

    Create ListExc4.hs

---
+++ ListExc4.hs
@@ -0,0 +1,5 @@
+-- Create a function hasPath that determines if a path from
+-- one node to another exists within a directed graph (check the video
+-- for the graph explanation)
+
+hasPath :: [(Int,Int)] -> Int -> Int -> Bool
